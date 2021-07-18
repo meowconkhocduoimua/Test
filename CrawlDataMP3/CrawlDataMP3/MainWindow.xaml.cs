56d7d1e49637cdacfeee4aa50520eb35adb2e8bb
meowconkhocduoimua <79196638+meowconkhocduoimua@users.noreply.github.com>
2021-07-18 20:25:16 +0700

    Add files via upload

---
+++ CrawlDataMP3/CrawlDataMP3/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CrawlDataMP3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private bool isCheckVN;
+        private bool isCheckEU;
+        private bool isCheckKO;
+        public bool IsCheckVN { get => isCheckVN; set { isCheckVN = value; isCheckEU = false; isCheckKO = false; OnPropertyChanged("IsCheckVN"); OnPropertyChanged("IsCheckEU"); OnPropertyChanged("IsCheckKO"); } }
+        public bool IsCheckEU { get => isCheckEU; set { isCheckEU = value; isCheckVN = false; isCheckKO = false; OnPropertyChanged("IsCheckVN"); OnPropertyChanged("IsCheckEU"); OnPropertyChanged("IsCheckKO"); } }
+        public bool IsCheckKO { get => isCheckKO; set { isCheckKO = value; isCheckEU = false; isCheckVN = false; OnPropertyChanged("IsCheckVN"); OnPropertyChanged("IsCheckEU"); OnPropertyChanged("IsCheckKO"); } }
+        public MainWindow()
+        {
+            InitializeComponent();
+            gridSongInfo.BackToMain += GridSongInfo_BackToMain;
+            lsbTopSongs.ItemsSource = new List<string>() { "", "", "", "", "", "", "", "", "", "" };
+
+            this.DataContext = this;
+            IsCheckVN = true;
+        }
+
+        private void GridSongInfo_BackToMain(object sender, EventArgs e)
+        {
+            GridTop10.Visibility = Visibility.Visible;
+            gridSongInfo.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GridTop10.Visibility = Visibility.Hidden;
+            gridSongInfo.Visibility = Visibility.Visible;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string newName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(newName));
+            }
+        }
+    }
+}
